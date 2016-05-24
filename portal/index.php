8bee8439fdc5016ceb9802f90445583880619378
Quinten Peels <Quinten@Localhost.pe>
2016-05-25 01:48:49 +0200

    portal index

---
+++ portal/index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+    name="viewport"><!-- Site Properties -->
+    <title>Simple Data</title>
+    <link href="../dist/components/reset.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/site.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/container.css" rel="stylesheet" type=
+    "text/css">
+    <link href="../dist/components/grid.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/header.css" rel="stylesheet" type=
+    "text/css">
+    <link href="../dist/components/image.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/button.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/shape.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/breadcrumb.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/label.css" rel="stylesheet" type="text/css">
+
+    <link href="../dist/components/menu.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/divider.css" rel="stylesheet" type=
+    "text/css">
+    <link href="../dist/components/list.css" rel="stylesheet" type="text/css">
+    <link href="../dist/components/segment.css" rel="stylesheet" type=
+    "text/css">
+    <link href="../dist/components/dropdown.css" rel="stylesheet" type=
+    "text/css">
+    <link href="../dist/components/icon.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    body {
+    background-color: #FFFFFF;
+    }
+    .ui.menu .item img.logo {
+    margin-right: 1.5em;
+    }
+    .main.container {
+    margin-top: 7em;
+    }
+    .wireframe {
+    margin-top: 2em;
+    }
+    .ui.footer.segment {
+    margin: 5em 0em 0em;
+    padding: 5em 0em;
+    }
+    </style>
+</head>
+<body>
+    <div class="ui fixed inverted menu">
+        <div class="ui container">
+            <a class="header item" href="#">Simple Data</a> <a class="item"
+            href="#">Home</a> <a class="item" href="#">Messages
+            <div class="floating ui red label">
+                4
+            </div></a> <a class="item" href="#">Activity</a>
+            <div class="right menu">
+                <div class="ui simple dropdown item">
+                    <img class="ui avatar image" src=
+                    "https://cnt.bet9ja.com/super9ja/img/login-user-icon.png"><span>Username</span><i class="dropdown icon"></i>
+                    <div class="menu">
+                        <div class="header">
+                            Profile
+                        </div><a class="item" href="#">Datastore</a> <a class=
+                        "item" href="#">User listing</a> <a class="item" href=
+                        "#">Send card</a>
+                        <div class="divider"></div><a class="item" href=
+                        "#">Settings</a> <a class="item" href="#">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui main container">
+
+        <div class="ui grid">
+            <div class="two wide column">
+                <div class="ui breadcrumb">
+                    <a class="section">Home</a>
+                    <div class="divider">
+                        /
+                    </div><a class="section">Store</a>
+                    <div class="divider">
+                        /
+                    </div>
+                    <div class="active section">
+                        T-Shirt
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ui grid">
+  <div class="three column row">
+    <div class="column">
+      <div class="ui cube shape">
+        <div class="sides">
+          <div class="active side">
+            <div class="content">
+              <div class="center">
+                <i class="alarm icon"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+     <div class="ui cube shape">
+        <div class="sides">
+          <div class="active side">
+            <div class="content">
+              <div class="center">
+                <i class="protect icon"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+     <div class="ui cube shape">
+        <div class="sides">
+          <div class="active side">
+            <div class="content">
+              <div class="center">
+                <i class="send icon"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+        
+
+
+
+    </div>
+    <div class="ui inverted vertical footer segment">
+        <div class="ui center aligned container">
+            <div class="ui stackable inverted divided grid">
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 1</h4>
+                    <div class="ui inverted link list">
+                        <a class="item" href="#">Link One</a> <a class="item"
+                        href="#">Link Two</a> <a class="item" href="#">Link
+                        Three</a> <a class="item" href="#">Link Four</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 2</h4>
+                    <div class="ui inverted link list">
+                        <a class="item" href="#">Link One</a> <a class="item"
+                        href="#">Link Two</a> <a class="item" href="#">Link
+                        Three</a> <a class="item" href="#">Link Four</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">Group 3</h4>
+                    <div class="ui inverted link list">
+                        <a class="item" href="#">Link One</a> <a class="item"
+                        href="#">Link Two</a> <a class="item" href="#">Link
+                        Three</a> <a class="item" href="#">Link Four</a>
+                    </div>
+                </div>
+                <div class="seven wide column">
+                    <h4 class="ui inverted header">Footer Header</h4>
+                    <p>Extra space for a call to action inside the footer that
+                    could help re-engage users.</p>
+                </div>
+            </div>
+            <div class="ui inverted section divider"></div><img class=
+            "ui centered mini image" src="../assets/images/logo.png">
+            <div class="ui horizontal inverted small divided link list">
+                <a class="item" href="#">Site Map</a> <a class="item" href=
+                "#">Contact Us</a> <a class="item" href="#">Terms and
+                Conditions</a> <a class="item" href="#">Privacy Policy</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
