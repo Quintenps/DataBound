2c2b47565140db4b0e057d9c3c0ee9c6918bdd11
Quinten Peels <Quinten@Localhost.pe>
2016-05-26 22:18:32 +0200

    menu include

---
+++ portal/includes/footer.php
@@ -0,0 +1,26 @@
+  <div class="ui inverted vertical footer segment">
+    <div class="ui container">
+      <div class="ui stackable inverted divided equal height stackable grid">
+        <div class="three wide column">
+          <h4 class="ui inverted header">About</h4>
+          <div class="ui inverted link list">
+            <a href="#" class="item">Front page</a>
+            <a href="#" class="item">Documentation</a>
+            <a href="#" class="item">Share Cards</a>
+            <a href="#" class="item">My datastore</a>
+          </div>
+        </div>
+        <div class="three wide column">
+          <h4 class="ui inverted header">Services</h4>
+          <div class="ui inverted link list">
+            <a href="#" class="item">Messages</a>
+            <a href="#" class="item">Activity</a>
+          </div>
+        </div>
+        <div class="seven wide column">
+          <h4 class="ui inverted header">Hogeschool Utrecht - ipas</h4>
+          <p>Invidueel propedeuse assignment.<br />Project gemaakt door Quinten Peels - V1D</p>
+        </div>
+      </div>
+    </div>
+  </div>
