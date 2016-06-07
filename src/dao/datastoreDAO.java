ac1b661f38bef79b99434f0b069d12e91a3425b8
Quinten Peels <Quinten@Peels.me>
2016-06-07 16:10:41 +0200

    sql prep change

--- src/dao/datastoreDAO.java
+++ src/dao/datastoreDAO.java
@@ -12,34 +12,49 @@ import java.sql.SQLException;
  */
 public class datastoreDAO extends baseDAO {
 
-    public void updateData(String df, int sesid) {
+    public void updateFirstName(String df, int sesid) {
 
         try (Connection con = super.getConnection()) {
 
             String datafield = df;
             int sessionid = sesid;
-            System.out.println("UPDATE datastore set "+datafield+" datafield");
 
-            PreparedStatement statement = con.prepareStatement("UPDATE datastore SET ?=? WHERE userid=?");
+            PreparedStatement statement = con.prepareStatement("UPDATE datastore SET firstname=? WHERE userid=?");
             statement.setString(1, datafield);
-            statement.setString(2, datafield);
-            statement.setInt(3, sessionid);
+            statement.setInt(2, sessionid);
             System.out.println(statement.executeUpdate());
-//
-//            ResultSet sqlresult = statement.executeQuery();
-//            sqlresult.next();
-//            int uid = (Integer) sqlresult.getObject("userid");
-//
-//            PreparedStatement addds = con.prepareStatement("INSERT INTO datastore (userid) VALUES (?) ");
-//            addds.setInt(1, uid);
-//            addds.executeUpdate();
-//            System.out.println("Datastore added!");
-//
-//            PreparedStatement addcd = con.prepareStatement("INSERT INTO cards (userid,cardname) VALUES (?,'FamilyCard'),(?,'BusinessCard'),(?,'UnknownCard') ");
-//            addcd.setInt(1, uid);
-//            addcd.setInt(2, uid);
-//            addcd.setInt(3, uid);
-//            addcd.executeUpdate();
+            System.out.println("Cards updated!");
+
+
+        }catch (SQLException sqle) { sqle.printStackTrace(); }
+    }
+    public void updateMiddleName(String df, int sesid) {
+
+        try (Connection con = super.getConnection()) {
+
+            String datafield = df;
+            int sessionid = sesid;
+
+            PreparedStatement statement = con.prepareStatement("UPDATE datastore SET middlename=? WHERE userid=?");
+            statement.setString(1, datafield);
+            statement.setInt(2, sessionid);
+            System.out.println(statement.executeUpdate());
+            System.out.println("Cards updated!");
+
+
+        }catch (SQLException sqle) { sqle.printStackTrace(); }
+    }
+    public void updateLastName(String df, int sesid) {
+
+        try (Connection con = super.getConnection()) {
+
+            String datafield = df;
+            int sessionid = sesid;
+
+            PreparedStatement statement = con.prepareStatement("UPDATE datastore SET lastname=? WHERE userid=?");
+            statement.setString(1, datafield);
+            statement.setInt(2, sessionid);
+            System.out.println(statement.executeUpdate());
             System.out.println("Cards updated!");
 
 
--- src/platform/DataStore.java
+++ src/platform/DataStore.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by 187z on 6/7/2016.
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 public class DataStore extends HttpServlet {
     private dao.datastoreDAO datastoredao;
     ArrayList<String> postedData = new ArrayList<>();
+    HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
     public void addtoList(String add){
         if (!add.equals("")) {
@@ -44,23 +45,51 @@ public class DataStore extends HttpServlet {
         String website = req.getParameter("website");
 
 
-        addtoList(firstname);
-        addtoList(middlename);
-        addtoList(lastname);
-        addtoList(gender);
-        addtoList(dateofbirth);
-        addtoList(country);
-        addtoList(town);
-        addtoList(facebook);
-        addtoList(twitter);
-        addtoList(skype);
-        addtoList(emailpersonal);
-        addtoList(emailbusiness);
-        addtoList(website);
+//        addtoList(firstname);
+//        addtoList(middlename);
+//        addtoList(lastname);
+//        addtoList(gender);
+//        addtoList(dateofbirth);
+//        addtoList(country);
+//        addtoList(town);
+//        addtoList(facebook);
+//        addtoList(twitter);
+//        addtoList(skype);
+//        addtoList(emailpersonal);
+//        addtoList(emailbusiness);
+//        addtoList(website);
 
-        for(String item:postedData){
-            datastoredao.updateData(item, (Integer)req.getSession().getAttribute("uid"));
+        hmap.put(1, firstname);
+        hmap.put(2, middlename);
+        hmap.put(3, lastname);
+        hmap.put(4, gender);
+        hmap.put(5, dateofbirth);
+        hmap.put(6, country);
+        hmap.put(7, city);
+        hmap.put(8, town);
+        hmap.put(9, facebook);
+        hmap.put(10, twitter);
+        hmap.put(11, skype);
+        hmap.put(12, emailpersonal);
+        hmap.put(13, emailbusiness);
+        hmap.put(14, website);
+
+        Set set = hmap.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry)iterator.next();
+            if (!mentry.getValue().equals(null) && !mentry.getValue().equals(""))
+            if((Integer) mentry.getKey() == 1) {
+                datastoredao.updateFirstName((String) mentry.getValue(), (Integer) req.getSession().getAttribute("uid"));
+            }
+            if((Integer) mentry.getKey() == 2) {
+                datastoredao.updateMiddleName((String) mentry.getValue(), (Integer) req.getSession().getAttribute("uid"));
+            }
+            if((Integer) mentry.getKey() == 3) {
+                datastoredao.updateLastName((String) mentry.getValue(), (Integer) req.getSession().getAttribute("uid"));
+            }
         }
+
 
 
         RequestDispatcher rd = req.getRequestDispatcher("/portal/datastore.jsp");
